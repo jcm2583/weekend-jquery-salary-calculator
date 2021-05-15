d11e0a93ddcbd9688ddb6cd9cb080417fff97a9a
Jacob Motes <jmotes2583@gmail.com>
2021-05-15 13:39:24 -0500

    total salary function is working

--- client.js
+++ client.js
@@ -10,6 +10,7 @@ console.log('jq is working!');
 
 // click listeners
 $ ('#submitButton').on('click', submitInfo)
+$ ('#submitButton').on('click', calculateMonthlyCosts)
 
 
 
@@ -39,6 +40,7 @@ let employeeObj = {
     $('#annualSalaryInput').val('');
     //console log employeeObj to make sure it is working
     console.log(employeeObj);
+    totalMonthlyCosts.push(employeeObj);
     //append the employee input info to the DOM
     $ ("#tableOutput").append(`<tr>
         <td>${employeeObj.firstName}</td>
@@ -53,17 +55,20 @@ let employeeObj = {
 //TODO
 // 1. Calculate monthly costs and append to the DOM
 
+let totalMonthlyCosts = []; 
+
 function calculateMonthlyCosts () {
     // I want to target the annual salary // employeeObj.annualSalary
     // I want to add the annual salaries together 
     // The added annual salaries need to go to an input that keeps track of the totals
     let monthlyCosts = 0;
-    let salary = employeeObj.annualSalary;
+    for (let i=0; i<totalMonthlyCosts.length; i++) {
+        monthlyCosts += totalMonthlyCosts[i].annualSalary / 12;
 
-        for (let i=0; i<salary.length; i++) {
-            monthlyCosts += salary[i];
+
     }
-    return monthlyCosts;
+    // $ ('#totalMonthlyCost').append(monthlyCosts);
+    console.log(('salary total', monthlyCosts));
+    $ ('#totalMonthlyCost').text(`Total Monthly Costs: $${monthlyCosts}`);
 }
 
-console.log(calculateMonthlyCosts());
